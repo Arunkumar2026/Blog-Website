9da1cc034e686f69836a450c1c78af6a7e69e757
Arun Kumar <akulakumar453@gmail.com>
2026-05-13 13:14:30 +0530

    this is backend API routes folder

---
+++ backend/routes/blogRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { createBlog, getBlogs, getBlogById, updateBlog, deleteBlog } from '../controllers/blogController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.post("/", protect, createBlog);
+router.get("/", getBlogs);
+router.get("/:id", getBlogById);
+router.put("/:id",protect, updateBlog);
+router.delete("/:id",protect, deleteBlog);
+
+
+export default router;

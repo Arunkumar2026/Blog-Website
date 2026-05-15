7324672dcd699f4f98018af6bf82e48e228b7c38
Arun Kumar <akulakumar453@gmail.com>
2026-05-15 15:18:36 +0530

    this is navbar file

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,40 +1,145 @@
-import { Link } from "react-router-dom"
-import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 
+import { Menu } from 'lucide-react';
+
 const Navbar = () => {
+
+  const user = JSON.parse(
+    localStorage.getItem("user")
+  );
+
+  const handleLogout = () => {
+
+    localStorage.removeItem("token");
+
+    localStorage.removeItem("user");
+
+    toast.success("Logged out");
+
+    window.location.reload();
+
+  };
+
   return (
-    <div className="navbar bg-base-100 shadow-md px-4 md:px-10">
-        <div className="flex-1">
-            <Link to="/" className="text-2xl font-bold text-primary">My Blogs</Link>
-        </div>
-        <div className="hidden md:flex gap-4 items-center">
-            <Link to="/" className="btn btn-ghost">Home</Link>
-            <Link to="/login" className="btn btn-ghost">Login</Link>
-            <Link to="/signup" className="btn btn-ghost">Signup</Link>
-            <Link to="/create" className="btn btn-primary">Create Blog</Link>
+    <div className="navbar bg-base-100 shadow-md px-6">
+
+      {/* Left */}
+      <div className="navbar-start">
+
+        {/* Mobile Menu */}
+        <div className="dropdown">
+
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden"
+          >
+
+            {<Menu/>}
+
+          </div>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+
+            <li>
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+
+            {
+              user && (
+                <li>
+                  <Link to="/create">
+                    Create Blog
+                  </Link>
+                </li>
+              )
+            }
+
+          </ul>
+
         </div>
 
-        <div className="dropdown dropdown-end md:hidden">
-            <label tabIndex={0} className="btn btn-ghost">{<GiHamburgerMenu/>}</label>
-            <ul tabIndex={0} className="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/singup">Singup</Link>
-                </li>
-                <li>
-                    <Link to="/create">Create Blog</Link>
-                </li>
-            </ul>
-        </div>
+        <Link
+          to="/"
+          className="text-2xl font-bold text-primary"
+        >
+          Blog App
+        </Link>
+
+      </div>
+
+      {/* Center */}
+
+      <div className="navbar-center hidden lg:flex">
+
+        <ul className="menu menu-horizontal px-1">
+
+          <li>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+
+          {
+            user && (
+              <li>
+                <Link to="/create">
+                  Create Blog
+                </Link>
+              </li>
+            )
+          }
+
+        </ul>
+
+      </div>
+
+
+      {/* Right */}
+
+      <div className="navbar-end gap-2">
+
+        {
+          user ? (
+
+            <button
+              onClick={handleLogout}
+              className="btn btn-error"
+            >
+              Logout
+            </button>
+
+          ) : (
+
+            <>
+              <Link
+                to="/login"
+                className="btn btn-outline"
+              >
+                Login
+              </Link>
+
+              <Link
+                to="/signup"
+                className="btn btn-primary"
+              >
+                Signup
+              </Link>
+            </>
+
+          )
+        }
+
+      </div>
+
     </div>
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
